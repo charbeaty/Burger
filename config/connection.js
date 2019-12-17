cb1ca576d9c68ef6d6a62823d62718f2c7628d74
Charissa Beaty <char.beaty11@gmail.com>
2019-12-16 17:57:22 -0700

    added connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection; 
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+ connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "P@izley2013",
   database: "burgers_db"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
